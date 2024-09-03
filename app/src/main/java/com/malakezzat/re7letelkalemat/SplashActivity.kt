a71dff739d660798e550449175ccbbe5e207dad5
mostafa <amgedtamer12345@gmail.com>
2024-09-03 10:37:58 +0300

    start

--- app/src/main/java/com/malakezzat/re7letelkalemat/SplashActivity.kt
+++ app/src/main/java/com/malakezzat/re7letelkalemat/SplashActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
     }
 }
