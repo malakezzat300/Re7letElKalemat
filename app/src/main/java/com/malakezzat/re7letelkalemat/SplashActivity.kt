5de004332f861c296f8e27493fb53633f8f22e7d
mostafa <amgedtamer12345@gmail.com>
2024-09-03 10:59:20 +0300

    start

--- app/src/main/java/com/malakezzat/re7letelkalemat/SplashActivity.kt
+++ app/src/main/java/com/malakezzat/re7letelkalemat/SplashActivity.kt
@@ -2,11 +2,14 @@ package com.malakezzat.re7letelkalemat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.malakezzat.re7letelkalemat.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var db: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        db = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(db.root)
 
     }
 }
