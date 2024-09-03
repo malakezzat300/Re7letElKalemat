ad6ea41d493c361c0d699f09a12fb85024201131
Nour <noureldin0@hotmail.com>
2024-09-03 07:58:16 +0300

    Splash Activity- Auth Activity (WelcomeScreenFragment - SignUpFragment - LogInFragment) - HomeActivity

--- app/src/main/java/com/malakezzat/re7letelkalemat/SplashActivity.kt
+++ app/src/main/java/com/malakezzat/re7letelkalemat/SplashActivity.kt
@@ -1,12 +1,9 @@
 package com.malakezzat.re7letelkalemat
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
