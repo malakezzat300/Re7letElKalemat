28cb02c3da5e7b1e02e4855669dadde6838e29df
malakezzat300 <47084913+malakezzat300@users.noreply.github.com>
2024-09-04 19:59:34 +0300

    Settings UI

--- app/src/main/java/com/malakezzat/re7letelkalemat/View/SettingsFragment.kt
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/SettingsFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.malakezzat.re7letelkalemat.R
+import com.malakezzat.re7letelkalemat.databinding.FragmentSettingsBinding
+import com.malakezzat.re7letelkalemat.databinding.FragmentWelcomeScreenBinding
 
 class SettingsFragment : Fragment() {
 
+    lateinit var db: FragmentSettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,12 +22,31 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        db = FragmentSettingsBinding.inflate(layoutInflater)
+        return db.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        db.notificationButton.setOnClickListener {
+            Toast.makeText(context,"notification",Toast.LENGTH_SHORT).show()
+            TODO("notification screen")
+        }
+
+        db.nightButton.setOnClickListener {
+            Toast.makeText(context,"night",Toast.LENGTH_SHORT).show()
+            TODO("night screen")
+        }
+
+        db.helpCenterButton.setOnClickListener {
+            Toast.makeText(context,"helpCenter",Toast.LENGTH_SHORT).show()
+            TODO("helpCenter screen")
+        }
+
+        db.contactUsButton.setOnClickListener {
+            Toast.makeText(context,"contactUs",Toast.LENGTH_SHORT).show()
+            TODO("contactUs screen")
+        }
     }
 }
