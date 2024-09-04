73f800f5eeb3b44fb3f034212f02a808a3ce3684
malakezzat300 <47084913+malakezzat300@users.noreply.github.com>
2024-09-04 21:17:26 +0300

    Username Auth UI

--- app/src/main/java/com/malakezzat/re7letelkalemat/Model/AuthModel.kt
+++ app/src/main/java/com/malakezzat/re7letelkalemat/Model/AuthModel.kt
@@ -6,7 +6,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class AuthModel(context: Context) {
     private val mAuth = FirebaseAuth.getInstance()
@@ -22,11 +24,17 @@ class AuthModel(context: Context) {
     }
 
     fun createUserWithEmailAndPassword(
+        username: String?,
         email: String?,
         password: String?,
         listener: OnCompleteListener<AuthResult?>?
     ) {
         mAuth.createUserWithEmailAndPassword(email!!, password!!).addOnCompleteListener(listener!!)
+        // Sign up success, update UI with the signed-in user's information
+        val user: FirebaseUser? = mAuth.currentUser
+        val profileUpdates = UserProfileChangeRequest.Builder()
+            .setDisplayName(username)
+            .build()
     }
 
     fun signInWithGoogle(account: GoogleSignInAccount, listener: OnCompleteListener<AuthResult?>?) {
