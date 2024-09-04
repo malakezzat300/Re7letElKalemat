8e044e725a0b05772e141d97dcd72acaab759f6a
malakezzat300 <47084913+malakezzat300@users.noreply.github.com>
2024-09-04 19:39:59 +0300

    Settings UI

---
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package com.malakezzat.re7letelkalemat.View
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.malakezzat.re7letelkalemat.R
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
