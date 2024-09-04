7d148779a9e7667547a8efbffa3cfeb31487d293
mostafagamal-cell <76878167+mostafagamal-cell@users.noreply.github.com>
2024-09-04 19:36:08 +0300

    in char

---
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/ScoreFragment.kt
@@ -0,0 +1,20 @@
+package com.malakezzat.re7letelkalemat.View
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.malakezzat.re7letelkalemat.R
+
+class ScoreFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_score, container, false)
+    }
+}
