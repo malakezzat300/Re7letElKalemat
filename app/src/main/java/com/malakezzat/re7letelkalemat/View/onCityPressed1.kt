efa0295703155ecb224b9478242ce4325a278b11
Nour <noureldin0@hotmail.com>
2024-09-05 20:35:24 +0300

    First Commit onCityPressed1

---
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/onCityPressed1.kt
@@ -0,0 +1,15 @@
+package com.malakezzat.re7letelkalemat.View
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.malakezzat.re7letelkalemat.R
+
+class onCityPressed1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_city_pressed1)
+    }
+}
