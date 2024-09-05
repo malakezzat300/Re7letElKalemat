d2560c55d82ea654dd46377793108144c91be936
Nour <noureldin0@hotmail.com>
2024-09-06 01:38:04 +0300

    CardsFragment Bug

--- app/src/main/java/com/malakezzat/re7letelkalemat/View/HomeFragment.kt
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.malakezzat.re7letelkalemat
+package com.malakezzat.re7letelkalemat.View
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,16 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.airbnb.lottie.LottieAnimationView
-import com.malakezzat.re7letelkalemat.View.onCityPressed1
 import com.malakezzat.re7letelkalemat.databinding.FragmentHomeBinding
-import com.malakezzat.re7letelkalemat.databinding.FragmentProfileBinding
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 
 class HomeFragment : Fragment() {
