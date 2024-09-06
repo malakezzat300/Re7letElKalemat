7d6fbc3b6d18bc6e446cfd56d3dab7e23227a6b4
sammar <sammaralaa95@gmail.com>
2024-09-06 23:09:48 +0300

    AfterSuccessInGame layout

---
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/AfterSuccessInGame.kt
@@ -0,0 +1,21 @@
+package com.malakezzat.re7letelkalemat.View
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.malakezzat.re7letelkalemat.R
+
+class AfterSuccessInGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_after_success_in_game)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
