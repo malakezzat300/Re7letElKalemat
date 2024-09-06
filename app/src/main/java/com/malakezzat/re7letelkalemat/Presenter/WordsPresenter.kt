6f73376a681ff7c75132c19f493fb2bd403940db
mostafagamal-cell <76878167+mostafagamal-cell@users.noreply.github.com>
2024-09-07 00:41:03 +0300

    add card

--- app/src/main/java/com/malakezzat/re7letelkalemat/Presenter/WordsPresenter.kt
+++ app/src/main/java/com/malakezzat/re7letelkalemat/Presenter/WordsPresenter.kt
@@ -1,16 +1,28 @@
 package com.malakezzat.re7letelkalemat.Presenter
 
+import com.malakezzat.re7letelkalemat.Model.Word
 import com.malakezzat.re7letelkalemat.Model.wordsList
 
-class WordsPresenter(private val view: WordsContract.View) : WordsContract.Presenter {
-
+class WordsPresenter(private val view: WordsContract.View, var w:Word? =null) : WordsContract.Presenter {
     // el mafrood han3mel load l kelma wa7da .. 3amlt el function dy keda w a5ls 3ashan agrb bas (bas el mafrod ndelo parameter bel word el haygbha maslan)
     override fun loadWords() {
         try {
-            val words = wordsList
-            view.showWords(words)
+            if (w==null)
+                    w = wordsList.random()
+            view.showWords(w!!)
         } catch (e: Exception) {
             view.showError("Error loading words: ${e.message}")
         }
+    }
+
+    override fun check(v: List<String>) {
+            val ans= w!!.exampleSentence.split(" ")
+            for (i in ans.indices){
+                if (ans[i] != v[i]){
+                    view.showFail()
+                    return
+                }
+            }
+        view.showSuccess()
     }
 }
---
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/TestGameActivity.kt
@@ -0,0 +1,17 @@
+package com.malakezzat.re7letelkalemat.View
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.malakezzat.re7letelkalemat.R
+
+class TestGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_test_game)
+
+    }
+}
