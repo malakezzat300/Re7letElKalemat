81b57ec84d254a9bc7ff4c47986245dda8f9e0ff
Nour <noureldin0@hotmail.com>
2024-09-06 18:42:28 +0300

    Cards Fragment

--- app/src/main/java/com/malakezzat/re7letelkalemat/View/MyCardFragment.kt
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/MyCardFragment.kt
@@ -6,29 +6,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.malakezzat.re7letelkalemat.Model.Word
+import com.malakezzat.re7letelkalemat.Presenter.WordsContract
+import com.malakezzat.re7letelkalemat.Presenter.WordsPresenter
 import com.malakezzat.re7letelkalemat.R
 import com.malakezzat.re7letelkalemat.databinding.FragmentMyCardBinding
 
 class MyCardFragment : Fragment() {
-    lateinit var db:FragmentMyCardBinding
+    private lateinit var binding: FragmentMyCardBinding
+    //private lateinit var presenter: WordsPresenter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        db=FragmentMyCardBinding.inflate(layoutInflater)
-        return db.root
+        binding = FragmentMyCardBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter=MyCardAdapter {
-          //  Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-          //pass action
-        }
-        db.myCardRecycler.adapter=adapter
-        val e= resources.displayMetrics.widthPixels
-        db.myCardRecycler.addItemDecoration(SpacesItemDecoration(e/5,e/5))
+
+        //SEBOOOHA COMMENTED 3ASHAN LAMA NEGY NE3ML EL FAVOURITES MAN3ODESH NFKAR
+
+//        presenter = WordsPresenter(object : WordsContract.View {
+//            override fun showWords(words: List<Word>) {
+//                val wordStrings = words.map { it.word } // Convert List<Word> to List<String>
+//
+//                val adapter = MyCardAdapter { word ->
+//                    // Handle card click
+//                    Toast.makeText(requireContext(), word, Toast.LENGTH_SHORT).show()
+//                }
+//                binding.myCardRecycler.adapter = adapter
+//                adapter.submitList(wordStrings) // Submit the list of word strings
+//            }
+//
+//
+//            override fun showError(message: String) {
+//                Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+//            }
+//        })
+//
+//        // Load words
+//        presenter.loadWords()
+
+        val spacing = resources.displayMetrics.widthPixels / 7
+        binding.myCardRecycler.addItemDecoration(SpacesItemDecoration(spacing, spacing))
     }
-
-
 }
