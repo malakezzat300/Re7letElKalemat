933c07c2b284c18584dcf9e7946f4ea12605acc8
Nour <noureldin0@hotmail.com>
2024-09-07 10:56:19 +0300

    Mainefest changes

---
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/CardDetailsActivity.kt
@@ -0,0 +1,15 @@
+package com.malakezzat.re7letelkalemat.View
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.malakezzat.re7letelkalemat.R
+
+class CardDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_details)
+    }
+}
---
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/RewordFinishCityWordsActivity.kt
@@ -0,0 +1,61 @@
+package com.malakezzat.re7letelkalemat.View
+
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.airbnb.lottie.LottieAnimationView
+import com.malakezzat.re7letelkalemat.R
+import com.malakezzat.re7letelkalemat.databinding.ActivityRewordFinishCityWordsBinding
+
+class RewordFinishCityWordsActivity : AppCompatActivity() {
+    private lateinit var db: ActivityRewordFinishCityWordsBinding
+    private lateinit var splashAnimation: LottieAnimationView
+    private lateinit var splashAnimation2: LottieAnimationView
+    private lateinit var mediaPlayer: MediaPlayer
+    private var isActivityRunning = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        db = ActivityRewordFinishCityWordsBinding.inflate(layoutInflater)
+        setContentView(db.root)
+        splashAnimation = db.viewAnimator
+        splashAnimation2 = db.viewAnimator2
+        mediaPlayer = MediaPlayer.create(this, R.raw.ready)
+    }
+    override fun onResume() {
+        super.onResume()
+
+        // Add a delay before starting the media player and animation
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (isActivityRunning) {
+                mediaPlayer.start()
+                splashAnimation.loop(true)
+                splashAnimation.playAnimation()
+                splashAnimation2.loop(true)
+                splashAnimation2.playAnimation()
+            }
+        }, 1000) // 2-second delay
+
+        // Delay for transitioning to the HomeActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (isActivityRunning) {
+                splashAnimation.cancelAnimation()
+                val intent = Intent(this, OnCityPressed2::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 9500) // 10.5 seconds total delay
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        isActivityRunning = false
+        mediaPlayer.release()
+    }
+}
