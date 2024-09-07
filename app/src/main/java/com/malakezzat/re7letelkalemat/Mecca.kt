e601c2327bbd9c503afbeb90b6db52bad69984e4
sammar <sammaralaa95@gmail.com>
2024-09-07 15:36:13 +0300

    MeccaActivity implementation

--- app/src/main/java/com/malakezzat/re7letelkalemat/Mecca.kt
+++ app/src/main/java/com/malakezzat/re7letelkalemat/Mecca.kt
@@ -1,4 +1,4 @@
-package com.malakezzat.re7letelkalemat.View
+package com.malakezzat.re7letelkalemat
 
 import android.content.Intent
 import android.media.MediaPlayer
@@ -9,33 +9,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.malakezzat.re7letelkalemat.Mecca
-import com.malakezzat.re7letelkalemat.R
-import com.malakezzat.re7letelkalemat.databinding.ActivitySaudiArabiaBinding
+import com.malakezzat.re7letelkalemat.View.HomeActivity
+import com.malakezzat.re7letelkalemat.databinding.ActivityMeccaBinding
 
-class SaudiArabiaActivity : AppCompatActivity() {
-    lateinit var binding : ActivitySaudiArabiaBinding
+class Mecca : AppCompatActivity() {
+    lateinit var binding: ActivityMeccaBinding
     lateinit var mediaPlayer : MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySaudiArabiaBinding.inflate(layoutInflater)
+       binding = ActivityMeccaBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        mediaPlayer = MediaPlayer.create(this,R.raw.saudi_arabia_sound)
+        mediaPlayer = MediaPlayer.create(this,R.raw.mecca)
 
         Handler(Looper.getMainLooper()).postDelayed({
             mediaPlayer.start()
         },1000)
         Handler(Looper.getMainLooper()).postDelayed({
 
-            val intent = Intent(this@SaudiArabiaActivity, Mecca::class.java)
+            val intent = Intent(this@Mecca, HomeActivity::class.java)
             startActivity(intent)
             finish()
-        }, 11800)
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mediaPlayer.release()
+        }, 9500)
     }
 }
