6b7e6d075a9c9aa986250d27425ac714efe1e6c7
malakezzat300 <47084913+malakezzat300@users.noreply.github.com>
2024-09-12 15:07:17 +0300

    Fix CardDetails

--- app/src/main/java/com/malakezzat/re7letelkalemat/View/CardDetailsActivity.kt
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/CardDetailsActivity.kt
@@ -16,17 +16,18 @@ import com.malakezzat.re7letelkalemat.Model.wordsList
 import com.malakezzat.re7letelkalemat.R
 import com.malakezzat.re7letelkalemat.databinding.ActivityCardDetailsBinding
 
+
 class CardDetailsActivity : AppCompatActivity() {
     private lateinit var db: ActivityCardDetailsBinding
     private lateinit var backButton: ImageView
-    private var myService: MyCardDetailService? = null
+    private var myService: MyCardDetailService ? = null
     private var isBound = false
-     var pos:Int=0
+    var pos:Int=0
     lateinit var word:String
     lateinit var meaning:String
     lateinit var example:String
-     var sound:Int=0
-    lateinit var handler:Handler
+    var sound:Int=0
+    lateinit var handler: Handler
     private  val TAG = "CardDetailsActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +37,11 @@ class CardDetailsActivity : AppCompatActivity() {
         }else{
             getSharedPreferences(TAG, MODE_PRIVATE).edit().clear().apply()
         }
-
-        pos =getSharedPreferences("position", MODE_PRIVATE).getInt("position",0)
-       word= intent.getStringExtra("word").toString()
-       meaning= intent.getStringExtra("meaning").toString()
-       example= intent.getStringExtra("example").toString()
-       sound =intent.getIntExtra("sound",-1)
+        pos =getSharedPreferences(TAG, MODE_PRIVATE).getInt("position",0)
+        word= intent.getStringExtra("word").toString()
+        meaning= intent.getStringExtra("meaning").toString()
+        example= intent.getStringExtra("example").toString()
+        sound =intent.getIntExtra("sound",-1)
         db.textView12.text = word
         db.textView16.text =meaning
         db.textView18.text =example
@@ -102,7 +102,7 @@ class CardDetailsActivity : AppCompatActivity() {
         db.viewAnimator.cancelAnimation()
     }
     fun savePos(){
-         pos=myService!!.getCurrentPosition()
+        pos=myService!!.getCurrentPosition()
         getSharedPreferences(TAG, MODE_PRIVATE).edit().putInt("position", pos).apply()
     }
 
@@ -110,12 +110,14 @@ class CardDetailsActivity : AppCompatActivity() {
         super.onPause()
         savePos()
         myService?.stopSound()
+        db.viewAnimator.pauseAnimation()
     }
     override fun onResume() {
         super.onResume()
         pos=getSharedPreferences(TAG, MODE_PRIVATE).getInt("position",0)
         myService?.playSound(sound)
         myService?.seekTo(pos)
+        db.viewAnimator.resumeAnimation()
         handler.sendEmptyMessage(0)
     }
 }
