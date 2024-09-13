31a7a754f94b2324c0bf36ea1ea003eda4e48f86
malakezzat300 <47084913+malakezzat300@users.noreply.github.com>
2024-09-13 14:59:29 +0300

    Fixed

---
+++ app/src/main/java/com/malakezzat/re7letelkalemat/View/LeaderBoardAdapter.kt
@@ -0,0 +1,39 @@
+package com.malakezzat.re7letelkalemat.View
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.malakezzat.re7letelkalemat.Model.User
+import com.malakezzat.re7letelkalemat.R
+
+class LeaderBoardAdapter(
+    private val userList: List<User>
+) : RecyclerView.Adapter<LeaderBoardAdapter.ViewHolder>() {
+
+    // ViewHolder class to hold and recycle views
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val userName: TextView = itemView.findViewById(R.id.user_name)
+        val userScore: TextView = itemView.findViewById(R.id.scoreText)
+    }
+
+    // Inflate the item layout and create ViewHolder objects
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.leaderboard_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    // Bind the data to the views in each list item
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = userList[position]
+        holder.userName.text = user.name
+        holder.userScore.text = user.score.toString()
+    }
+
+    // Return the total number of list items
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+}
